1c53d68c30019f77c5ae939c1c5a70cac890ff3b
Requiem <94884947+ardiansyaht@users.noreply.github.com>
2024-03-03 13:50:35 +0700

    Update Work.jsx

--- components/Work.jsx
+++ components/Work.jsx
@@ -31,7 +31,7 @@ const projectData = [
     category: "React js",
     name: "Personal Website",
     description: "Portfolio ",
-    link: "https://portfolio-ardiansyaht.vercel.app/",
+    link: "https://portfolio-ardhiansyah.vercel.app/",
     github: "https://github.com/ardiansyaht/portfolio",
   },
   {
